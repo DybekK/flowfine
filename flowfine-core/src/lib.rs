cb433f20a0b92c51900827a92351d9617c7714e5
Mateusz Dybowski <matdybowski@gmail.com>
2023-09-03 21:36:38 +0200

    Introduce Github Actions (#5)
    * Set initial version as 0.0.1
    
    * Add Github Actions for tests

--- flowfine-core/src/lib.rs
+++ flowfine-core/src/lib.rs
@@ -1,12 +1,2 @@
-use crate::config::MigrationConfig;
-use crate::scanner::parser::parse_migrations;
-
 pub mod config;
 mod scanner;
-
-pub async fn migrate(config: MigrationConfig) {
-    match parse_migrations(&config.directory, &config.version_formatting) {
-        Ok(migration_result) => migration_result.print_report(),
-        Err(err) => println!("Error: {:?}", err),
-    }
-}
